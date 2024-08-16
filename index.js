3224137eece22113ee3425e22790bff0f7eee052
Sergio Martinez <elsamplio@msn.com>
2024-08-15 21:27:34 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const router = express.Router();
+const axios = require("axios");
+
+router.get("/", (req, res) => {
+  const { page = 1, limit = 10 } = req.query;
+  axios
+    .get("https://www.amiiboapi.com/api/amiibo")
+    .then((response) => {
+      const {
+        data: { amiibo },
+      } = response;
+      const pageNumber = Number(page);
+      const limitNumber = Number(limit);
+      const startIndex = (pageNumber - 1) * limitNumber;
+      const endIndex = startIndex + limitNumber;
+      res.status(200).send(amiibo.slice(startIndex, endIndex));
+    })
+    .catch((err) => res.status(500).send(err));
+});
+
+app.use(router);
+
+app.listen(3001, async () => {
+  console.log(`Server is running`);
+});
